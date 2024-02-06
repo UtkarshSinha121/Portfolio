4dac889237b0ad3e636f03fd02c3064265b259b9
UtkarshSinha121 <be20cs115utkarshsinha@gmail.com>
2024-02-06 22:52:31 +0530

    responsive

--- src/Component/Intro.jsx
+++ src/Component/Intro.jsx
@@ -3,20 +3,20 @@ import utk from '../Images/bg.png'
 
 const Intro = () => {
   return (
-    <div className='d-flex px-5'>
-        <div className='col-md-7 intro'>
+    <section className='intro'>
+        <div className='detail' >
            <h6 className='intropara1'>Hello,</h6>
-           <h3 className='intropara2'>I'm <span className='bgclr'> Utkarsh </span> </h3>
-           <p className='intropara3'>I am energetic, hardworking person seeking a position in a company where I can utilize my training and skills, while making a significant contribution to the succes of the company.</p>
+           <h3 className='intropara2'>I'm <span className='bgclr'> Utkarsh Sinha </span> </h3>
+           <p className='intropara3'>I am energetic, hardworking person seeking a position in a company <br /> where I can utilize my training and skills, <br /> while making a significant contribution to the succes of the company.</p>
            <div>
            <button className="btn btn-white rounded-5 desktopMenuBtn">
            <i class="fa-regular fa-message mx-2"></i>Contact Me</button>
            </div>
         </div>
-        <div className='col-md-5'>
-             
+        <div >
+             <img className='utk' src={utk} alt="" />
         </div>
-    </div>
+    </section>
   )
 }
 
